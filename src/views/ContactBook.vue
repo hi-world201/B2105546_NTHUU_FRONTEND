9f94169efb8ab7aad83eda999b8adedaa7b59c79
hi-world201 <90491406+hi-world201@users.noreply.github.com>
2023-10-27 08:08:14 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/views/ContactBook.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="page row">
+    <div class="col-md-10">
+      <input-search v-model="searchText"></input-search>
+    </div>
+    <div class="mt-3 col-md-6">
+      <h4>Danh bạ <i class="fas fa-address-book"></i></h4>
+
+      <contact-list
+        v-if="filteredContactsCound > 0"
+        :contacts="filteredContacts"
+        v-model:activeIndex="activeIndex"
+      ></contact-list>
+      <p v-else>Không có liên hệ nào</p>
+
+      <div class="mt-3 row d-flex justify-content-around align-items-center">
+        <button class="btn btn-sm btn-primary" @click="refreshList()">
+          <i class="fas fa-redo"></i> Làm mới
+        </button>
+        <button class="btn btn-sm btn-success" @click="goToAddContact()">
+          <i class="fas fa-plus"></i> Thêm mới
+        </button>
+
+        <button class="btn btn-sm btn-danger" @click="removeAllContacts">
+          <i class="fas fa-trash"></i> Xóa tất cả
+        </button>
+      </div>
+    </div>
+
+    <div class="mt-3 col-md-6">
+      <div v-if="activeContact">
+        <h4>Chi tiết liên hệ <i class="fas fa-address-card"></i></h4>
+        <contact-card :contact="activeContact"></contact-card>
+      </div>
+      <button
+        v-if="activeIndex !== -1"
+        class="btn btn-xs btn-warning"
+        style="padding: 0 2px; font-size: 12px; font-weight: 700"
+      >
+        <i class="fas fa-pen-to-square"></i>
+        Hiệu chỉnh
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContactCard from "@/components/ContactCard.vue";
+import InputSearch from "@/components/InputSearch.vue";
+import ContactList from "@/components/ContactList.vue";
+import ContactService from "@/services/contact.service.js";
+
+export default {
+  components: {
+    ContactCard,
+    InputSearch,
+    ContactList,
+  },
+
+  data() {
+    return {
+      contacts: [],
+      activeIndex: -1,
+      searchText: "",
+    };
+  },
+
+  watch: {
+    searchText() {
+      this.activeIndex = -1;
+    },
+  },
+
+  computed: {
+    contactStrings() {
+      return this.contacts.map((contact) => {
+        const { name, email, address, phone } = contact;
+        return [name, email, address, phone].join("");
+      });
+    },
+
+    filteredContacts() {
+      if (!this.searchText) return this.contacts;
+      return this.contacts.filter((_contact, index) =>
+        this.contactStrings[index].includes(this.searchText)
+      );
+    },
+
+    activeContact() {
+      if (this.activeIndex < 0) return null;
+      return this.filteredContacts[this.activeIndex];
+    },
+
+    filteredContactsCound() {
+      return this.filteredContacts.length;
+    },
+  },
+
+  methods: {
+    async retrieveContacts() {
+      try {
+        this.contacts = await ContactService.getAll();
+      } catch (err) {
+        console.log(error);
+      }
+    },
+
+    refreshList() {
+      this.retrieveContacts();
+      this.activeIndex = -1;
+      this.searchText = "";
+    },
+
+    async removeAllContacts() {
+      if (confirm("Bạn có muốn xóa tất cả liên hệ?")) {
+        try {
+          await ContactService.deleteAll();
+          this.refreshList();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    },
+
+    goToAddContact() {
+      this.$router.push({ name: "contact.add" });
+    },
+  },
+
+  mounted() {
+    this.refreshList();
+  },
+};
+</script>
+
+<style scoped>
+.page {
+  text-align: left;
+  max-width: 750px;
+  margin: auto;
+}
+</style>
